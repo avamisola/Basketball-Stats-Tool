dd2725f43dc60d8da41487769b8bfa5b8d1415bd
avamisola <58097632+avamisola@users.noreply.github.com>
2020-08-12 00:28:41 -0400

    create clean_data function

--- app.py
+++ app.py
@@ -3,5 +3,25 @@ Python Web Development Techdegree
 Project 2 - Basketball Stats Tool
 """
 
+
+from constants import TEAMS, PLAYERS
+
+
 if __name__ == "__main__":
-    print("hello world")
+
+
+    def clean_data():
+        """copy player data to new list, save height as integer, save experience as boolean"""
+
+        player_list = []
+
+        for player in PLAYERS:
+            player_list.append(player)
+
+        for player in player_list:
+            player["height"] = int(player["height"].replace(' inches', ''))
+            player["experience"] = bool(player["experience"].replace('NO', ''))
+
+        print(player_list)
+
+    clean_data()
